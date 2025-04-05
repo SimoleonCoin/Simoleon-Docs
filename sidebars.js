4242fe0c9ec6ae32eabee8aabb8ae39ea6af921b
SimoleonCoin <simoleonlabs@gmail.com>
2025-04-04 19:06:47 -0700

    Create sidebars.js

---
+++ sidebars.js
@@ -0,0 +1,28 @@
+/**
+ * Creating a sidebar enables you to:
+ - create an ordered group of docs
+ - render a sidebar for each doc of that group
+ - provide next/previous navigation
+
+ The sidebars can be generated from the filesystem, or explicitly defined here.
+ */
+
+module.exports = {
+  docs: [
+    {
+      type: 'doc',
+      id: 'getting-started', // document ID
+      label: 'Getting Started',
+    },
+    {
+      type: 'doc',
+      id: 'sdk',
+      label: 'Simoleon SDK',
+    },
+    {
+      type: 'doc',
+      id: 'api',
+      label: 'Simoleon API',
+    },
+  ],
+};
